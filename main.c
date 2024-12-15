7a28f2a23b33c9ffb26d0f019f3aeb750bfe3974
SDiHALF <shivamsharma22468@gmail.com>
2024-12-15 12:23:14 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
 
+#define assign(a, b) b; \
+  a = &lmd ## __LINE__;
+
+#define lambda(a, _, c) \
+  int lmd ## __LINE__(int a){return c;}
+
 int main(){
+  int (*p)(int);
+
+  assign(p, lambda(x, ->, x+1);
+
+  printf("%d", p(1));
   return 0;
 }
